5f08fda597edc4483b588c0ff1740139a7aa47cb
Naveen <naveen17593@gmail.com>
2026-01-30 22:32:40 +0530

    daily

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const toggle = document.getElementById("themeToggle");
 
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  toggle.textContent = "☀️";
+}
+
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    toggle.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggle.textContent = "🌙";
+  }
 });
-const l="Naveen";
